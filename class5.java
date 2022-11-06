5ba4af2d707831839b1462f59a0f073ef763f18c
luckly-xixi <105301098+luckly-xixi@users.noreply.github.com>
2022-11-07 00:04:09 +0800

    作业

--- class5.java
+++ class5.java
@@ -2,7 +2,17 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class class5 {
 
-
+    public static void main(String[] args) {
+        int n = 5;
+        int ret = factor(n);
+        System.out.println("ret = " + ret);
+    }
+    public static int factor(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factor(n - 1); // factor 调用函数自身
+    }
 
     public static int getSum(int N){   // N是形参
         return (1+N)*N / 2;
--- work3.java
+++ work3.java
@@ -3,16 +3,35 @@ import java.util.Scanner;
 public class work3 {
 
     public static void main(String[] args) {
-        //按顺序打印一个数字的每一位(例如 1234 打印出 1 2 3 4) （递归）
-        int a = 1234;
+        //写一个递归方法，输入一个非负整数，返回组成它的数字之和
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
 
-        System.out.println(print(a));
+        int b = inc(a);
+        System.out.println(b);
     }
-    public static int print(int x) {
+    public static int inc(int x) {
+        int sum =0;
         if(x < 10) {
             return x;
         }
-        return print(x / 10);
+        sum = (x%10)+inc(x / 10);
+        return sum;
+    }
+
+    public static void main3(String[] args) {
+        //按顺序打印一个数字的每一位(例如 1234 打印出 1 2 3 4) （递归）
+        int a = 1234;
+
+        print(a);
+    }
+    public static void print(int x) {
+        if(x < 10) {
+            System.out.println(x);
+            return ;
+        }
+        print(x / 10);
+        System.out.println(x % 10);
     }
 
     public static void main2(String[] args) {
