5b42026bc338fbd9087928d3c178963217ffb2ea
luckly-xixi <105301098+luckly-xixi@users.noreply.github.com>
2022-11-07 10:52:34 +0800

    方法的使用（完结）
    方法的使用（完结）

--- class5.java
+++ class5.java
@@ -2,6 +2,26 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class class5 {
 
+    public static void main6(String[] args) {
+        hannuota(1,'a','b','c');
+        System.out.println();
+        hannuota(2,'a','b','c');
+        System.out.println();
+        hannuota(3,'a','b','c');
+    }
+    public static void hannuota(int n,char pos1,char pos2,char pos3) {
+        if(n == 1) {
+            move(pos1,pos3);
+            return;
+        }
+        hannuota(n-1,pos1,pos3,pos2);
+        move(pos1,pos3);
+        hannuota(n-1,pos2,pos1,pos3);
+    }
+    public static void move(char pos1,char pos2) {
+        System.out.print(pos1+"—》"+pos2+"    ");
+    }
+
     public static void main(String[] args) {
         int n = 5;
         int ret = factor(n);
