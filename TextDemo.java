4374f8ca7ae256de68e438ae182eee1732968678
luckly-xixi <105301098+luckly-xixi@users.noreply.github.com>
2022-11-13 20:28:11 +0800

    封包的认识
    封包的认识

--- TextDemo.java
+++ TextDemo.java
@@ -1,7 +1,11 @@
-package com.baodu.www2;
+package com.baodu.www;
 
 public class TextDemo {
-    int size = 99;
-    //但是我们在这没有给size加任何的权限关键字
-    //导致我们size只能在当前包使用（包访问权限）
+
+    public static void main(String[] args) {
+        com.baodu.www2.TextDemo textDemo = new com.baodu.www2.TextDemo();
+        //优先使用这个包自己的TextDemo
+//        System.out.println(textDemo.size);
+    }
+
 }
