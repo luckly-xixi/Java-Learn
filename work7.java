d0a50f5fac3ef5af9e7a223c28b79070cd088e1f
luckly-xixi <105301098+luckly-xixi@users.noreply.github.com>
2022-11-13 20:29:13 +0800

    类和对象的相关练习题
    类和对象的相关练习题

---
+++ work7.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Data {
+
+    private int x;
+    private int y;
+
+    public Data(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
+
+public class work7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            Data data = new Data(x, y);
+            System.out.println(data.getX() + data.getY());
+        }
+    }
+}
