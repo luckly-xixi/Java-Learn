2b8f2f4d8db9ede8ed22de884e7e45e6d31a5ebd
luckly-xixi <105301098+luckly-xixi@users.noreply.github.com>
2022-11-14 23:17:39 +0800

    继承和多态
    继承和多态

---
+++ class8.java
@@ -0,0 +1,132 @@
+
+
+// Dog.java
+
+/* class Dog{
+            String name;
+        int age;
+        float weight;
+
+        public void eat(){
+            System.out.println(name + "正在吃饭");
+        }
+
+        public void sleep(){
+            System.out.println(name + "正在睡觉");
+        }
+
+        void Bark(){
+            System.out.println(name + "汪汪汪~~~");
+        }
+    }*/
+
+
+
+
+    // Cat.Java
+
+/*class Cat{
+            String name;
+        int age;
+        float weight;
+
+        public void eat(){
+            System.out.println(name + "正在吃饭");
+        }
+
+        public void sleep()
+        {
+            System.out.println(name + "正在睡觉");
+        }
+
+        void mew(){
+            System.out.println(name + "喵喵喵~~~");
+        }
+    }*/
+
+     class Animal{
+    String name;
+    int age;
+
+    public void eat(){
+        System.out.println(name + "正在吃饭");
+    }
+
+    public void sleep(){
+        System.out.println(name + "正在睡觉");
+    }
+}
+
+// Dog.java
+    class Dog extends Animal{
+    void bark(){
+        System.out.println(name + "汪汪汪~~~");
+    }
+}
+// Cat.Java
+    class Cat extends Animal{
+    void mew(){
+        System.out.println(name + "喵喵喵~~~");
+    }
+}
+
+    class Beas {
+         public int a;
+
+         public int b;
+
+         int c = 9;
+    }
+
+    class Derived extends Beas {
+         //子类和父类不同命
+         public int c;
+
+         public void func(){
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            ////子类有就使用子类的，子类没有就使用父类的
+        }
+    }
+
+class Derive extends Beas {
+    //子类和父类不同命
+    public int c = 6;
+
+    public void func(){
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        ////子类有就使用子类的，子类没有就使用父类的
+    }
+}
+
+    public class class8 {
+
+         //父类成员访问
+        public static void main(String[] args) {
+        Derive derive = new Derive();
+        derive.func();
+        //如果父类和子类有同名的成员变量，优先访问自己的
+        }
+
+        public static void main2(String[] args) {
+            Derived derived = new Derived();
+            derived.func();
+        }
+
+        //继承语法的认识
+        public static void main1(String[] args) {
+            Dog dog = new Dog();
+            dog.name = "菜狗";
+            dog.bark();
+            dog.eat();
+
+            Cat cat = new Cat();
+            cat.name = "八嘎猫";
+            cat.mew();
+            cat.eat();
+        }
+
+}
