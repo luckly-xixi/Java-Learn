ce7746b5f9cb7d32bc0d9f6641004a32454d514a
luckly-xixi <105301098+luckly-xixi@users.noreply.github.com>
2022-11-17 23:19:46 +0800

    Add files via upload

--- class8.java
+++ class8.java
@@ -109,9 +109,72 @@ class Derived extends Beas {
         //子类有就使用子类的，子类没有就使用父类的
     }
 }
+class Father {
+        public int a = 6 ;
+        public float b;
+        public String c;
+    public Father(int a,  String c) {
+        this.a = a;
+        this.c = c;
+        System.out.println("Father带两个参数的构造方法");
+    }
+    public Father() {
+        System.out.println("Father()");
+    }
+    static {
+        System.out.println("Father static{}");
+    }
+    {
+        System.out.println("Father {}");
+    }
+    public void methodA() {
+        System.out.println("Father当中的方法A");
+    }
+    public void methodB() {
+        System.out.println("Father当中的方法B");
+    }
+}
+class Son extends Father {
+         public char d;
+         public int a = 5 ;
+        public Son (){
+        //里面虽然什么都没加，但是编译器默认认为调用父类的无参数构造方法
+            //super（）{} 编译器默认帮你加上
+            System.out.println("Son()");
+        }
+    public void methodA() {
+        System.out.println("Son当中的方法A");
+    }
+    static {
+        System.out.println("Son static {}");
+    }
+    {
+        System.out.println("Son {}");
+    }
+}
     public class class8 {
+        public static void main7(String[] args) {
+            Father father1 = new Father();
+            System.out.println("=======");
+            Father father2 = new Father();
+        }
+
+        public static void main(String[] args) {
+            Son son1 = new Son();
+            System.out.println("======");
+            Son son2 = new Son();
+            System.out.println("--------");
+            Father father1 = new Father();
+            System.out.println("=======");
+            Father father2 = new Father();
+        }
+
+        public static void main5(String[] args) {
+            Father father =new Father(10,"giao");
+            Son son = new Son();
+        }
          //父类和子类当中的方法同名
-         public static void main(String[] args) {
+         public static void main4(String[] args) {
              Derive derive = new Derive();
              derive.methodA();
              derive.func();
