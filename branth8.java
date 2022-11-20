e6739e1620dfe5cda015507eb6921bc5de5e7bdf
luckly-xixi <105301098+luckly-xixi@users.noreply.github.com>
2022-11-20 13:01:30 +0800

    多态（已完结）
    多态（已完结）

--- branth8.java
+++ branth8.java
@@ -32,24 +32,129 @@ class Cats extends Animals {
         System.out.println(name+"正在吃猫粮！");
     }
 }
+
+class Shape {
+    //属性....
+    public void draw() {
+        System.out.println("画图形！");
+    }
+}
+class Rect extends Shape{
+    @Override
+    public void draw() {
+        System.out.println("♦");
+    }
+}
+class Cycle extends Shape{
+    @Override
+    public void draw() {
+        System.out.println("●");
+    }
+}
+class Flower extends Shape{
+    @Override
+    public void draw() {
+        System.out.println("❀");
+    }
+}
+
+class B {
+    public B() {
+        // do nothing
+        func();
+    }
+    public void func() {
+        System.out.println("B.func()");
+    }
+}
+class D extends B {
+    private int num = 1;
+    @Override
+    public void func() {
+        System.out.println("D.func() " + num);
+    }
+}
+
 public class branth8 {
+
+    public static void main(String[] args) {
+        D d = new D();
+    }
+
+    //圈复杂度（不基于多态）
+    public static void drawShapes() {
+        Rect rect = new Rect();
+        Cycle cycle = new Cycle();
+        Flower flower = new Flower();
+        String[] shapes = {"cycle", "rect", "cycle", "rect", "flower"};
+
+        for (String shape : shapes) {
+            if (shape.equals("cycle")) {
+                cycle.draw();
+            } else if (shape.equals("rect")) {
+                rect.draw();
+            } else if (shape.equals("flower")) {
+                flower.draw();
+            }
+        }
+    }
+
+    //圈复杂度（多态）
+
+    static void drawShapes2() {
+        Rect rect = new Rect();
+        Cycle cycle = new Cycle();
+        Flower flower = new Flower();
+
+        Shape[] shapes ={rect,cycle,rect,cycle,flower};
+
+        for (Shape shape:shapes) {
+            shape.draw();
+        }
+    }
+
+    public static void main6(String[] args) {
+//        drawShapes();
+        drawShapes2();
+    }
+
+    //向下转型
+    public static void main5(String[] args) {
+        Animals animals = new Dogs();
+
+        Dogs dogs = (Dogs) animals;
+        dogs.Call();
+
+        System.out.println("==============");
+
+       if (animals instanceof Cats) {
+           Cats cats = (Cats) animals;
+           cats.Run();
+       }
+    }
+
 
     //向上转型三：方法的返回值
     public static Animals func2() {
         return new Dogs();
     }
 
-    public static void main(String[] args) {
+    public static void function(Animals animals) {
+        animals.eat();
+    }
+
+    public static void main4(String[] args) {
 
         Animals animals1 = new Dogs();
         animals1.name = "菜狗";
-        animals1.eat();
-
+//        animals1.eat();
+        function(animals1);
         System.out.println("==============");
 
         Animals animals2 = new Cats();
         animals2.name= "八嘎猫";
-        animals2.eat();
+//        animals2.eat();
+        function(animals2);
     }
 
     public static void func(Animals animals1 ) {
