9a8febac1a4e93687bd9510f8e989fea2536d372
万西西 <1348673083@qq.com>
2022-11-20 19:05:19 +0800

    包的概念

---
+++ com/baodu/www/TextDemo1.java
@@ -0,0 +1,13 @@
+package com.baodu.www;
+
+import com.baodu.www2.TextDemo2;
+
+public class TextDemo1 {
+
+    public static void main(String[] args) {
+        TextDemo2 textDemo = new TextDemo2();
+        //优先使用这个包自己的TextDemo
+//        System.out.println(textDemo.size);
+    }
+
+}
