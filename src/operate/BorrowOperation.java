dc82c7f819ae46c9783a570d93b9f36f9de513a6
万西西 <1348673083@qq.com>
2022-11-29 22:30:27 +0800

    图书管理系统（功能实现4）

--- src/operate/BorrowOperation.java
+++ src/operate/BorrowOperation.java
@@ -1,11 +1,30 @@
 package operate;
 
+import book.Book;
 import book.BookList;
+
+import java.util.Scanner;
 
 public class BorrowOperation implements IOperation{
 
     @Override
     public void work(BookList bookList) {
         System.out.println("借阅图书");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int currentSize = bookList.getUsedSize();
+
+        for (int i = 0; i < currentSize; i++) {
+
+            Book book = bookList.getBooks(i);
+
+            if(book.getName().equals(name)) {
+
+                System.out.println("找到该书：");
+                System.out.println(book);
+                return;
+            }
+        }
+        System.out.println("没有该书籍");
     }
 }
