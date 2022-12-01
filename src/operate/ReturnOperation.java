9298def3055145e247846d15e1baa784decf3a38
万西西 <1348673083@qq.com>
2022-12-01 23:21:51 +0800

    图书管理系统（功能实现6）

--- src/operate/ReturnOperation.java
+++ src/operate/ReturnOperation.java
@@ -1,11 +1,22 @@
 package operate;
 
+import book.Book;
 import book.BookList;
+
+import java.util.Scanner;
 
 public class ReturnOperation implements IOperation{
 
     @Override
     public void work(BookList bookList) {
         System.out.println("归还图书");
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入书名");
+        String name = scanner.nextLine();
+
+        int currentSize = bookList.getUsedSize();
+        for (int i = 0; i < currentSize; i++) {
+            Book tmp = bookList.getBooks(i);
+
+        }
 }
