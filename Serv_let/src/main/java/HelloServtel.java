7bc8f8af150117c12d68f4e191b23223b0f07c73
xixi <1348673083@qq.com>
2023-08-29 22:04:12 +0800

    Serlet中的HttpServlet

--- Serv_let/src/main/java/HelloServtel.java
+++ Serv_let/src/main/java/HelloServtel.java
@@ -11,7 +11,29 @@ public class HelloServtel extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
         System.out.println("hello world");
+//    告诉浏览器，返回的数据是utf-8格式的
+        resp.setContentType("text/heml;charser=utf8");
+        resp.getWriter().write("这是一个get");
 
-        resp.getWriter().write("hello world");
+
+//        resp.getWriter().write("hello world");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+    resp.getWriter().write("This is doPost");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPut(req, resp);
+    resp.getWriter().write("This is doPut");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doDelete(req, resp);
+    resp.getWriter().write("This is doDelete");
     }
 }
