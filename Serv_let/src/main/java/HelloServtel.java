591aa6beb9d5be9450bfc20e36e5875fc5ee1a58
xixi <1348673083@qq.com>
2023-08-29 21:01:46 +0800

    Servlet

---
+++ Serv_let/src/main/java/HelloServtel.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServtel extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        System.out.println("hello world");
+
+        resp.getWriter().write("hello world");
+    }
+}
