1e93688352a80710e2f6bf95f7c0a1b6b6cfb6f8
xixi <1348673083@qq.com>
2023-10-15 23:47:31 +0800

    1

--- coding/src/graph.java
+++ coding/src/graph.java
@@ -311,64 +311,101 @@ public class graph {
 
 
 
-    public static class NodeRecord {
-        public Node node;
-        public int distance;
-
-        public NodeRecord(Node node,int distance) {
-            this.node = node;
-            this.distance = distance;
-        }
-    }
-
-    public static class NodeHeap {
-        private Node[] nodes;
-        private HashMap<Node,Integer> heapIndexMap; // node 在堆上的位置
-        private HashMap<Node,Integer> distanceMap;
-        private int size;
-
-        public NodeHeap(int size) {
-            nodes = new Node[size];
-            heapIndexMap = new HashMap<>();
-            distanceMap = new HashMap<>();
-            this.size = 0;
-        }
-
-        public boolean isEmpty() {
-            return size == 0;
-        }
-
-        private boolean isEntered(Node node) {//  node是否进入过堆
-            return heapIndexMap.containsKey(node);
-        }
-
-        private boolean inHeap(Node node) { // node 是否在堆上
-            return isEntered(node) && heapIndexMap.get(node) != -1;
-        }
-
-        private void swap(int index1,int index2) {
-            heapIndexMap.put(nodes[index1],index2);
-            heapIndexMap.put(nodes[index2],index1);
-            Node temp = nodes[index1];
-            nodes[index1] = nodes[index2];
-            nodes[index2] = temp;
-        }
-    }
-
-// 改进后的dijkstra算法
-    public static HashMap<Node,Integer> dijkstra2(Node head,int size) {
-        NodeHeap nodeHeap = new NodeHeap(size);
-        nodeHeap.addOrUpdateOrIgnore(head,0);
-        HashMap<Node,Integer> result = new HashMap<>();
-        while(!nodeHeap.isEmpty()) {
-            NodeRecord record = nodeHeap.pop();
-            Node cur = record.node;
-            int distance = record.ditance;
-            for(Edge edge : cur.edges) {
-                nodeHeap.addOrUpdateOrIgnore(edge.to,edge.weight + distance);
-            }
-            result.put(cur,distance);
-        }
-        return result;
-    }
+//    public static class NodeRecord {
+//        public Node node;
+//        public int distance;
+//
+//        public NodeRecord(Node node,int distance) {
+//            this.node = node;
+//            this.distance = distance;
+//        }
+//    }
+//
+//    public static class NodeHeap {
+//        private Node[] nodes;
+//        private HashMap<Node,Integer> heapIndexMap; // node 在堆上的位置
+//        private HashMap<Node,Integer> distanceMap;
+//        private int size;
+//
+//        public NodeHeap(int size) {
+//            nodes = new Node[size];
+//            heapIndexMap = new HashMap<>();
+//            distanceMap = new HashMap<>();
+//            this.size = 0;
+//        }
+//
+//        public boolean isEmpty() {
+//            return size == 0;
+//        }
+//
+//        public void addOrUpdateOrIgnore(Node node,int distance) {
+//            if(inHeap(node)) {
+//                distanceMap.put(node,Math.min(distanceMap.get(node),distance));
+//                insertHeapify(node,heapIndexMap.get(node));
+//            }
+//            if(!isEntered(node)) {
+//                nodes[size] = node;
+//                heapIndexMap.put(node,size);
+//                distanceMap.put(node,distance);
+//                insertHeapifY(node,size);
+//            }
+//        }
+//
+//        public NodeRecord pop() {
+//            NodeRecord nodeRecord = new NodeRecord(nodes[0],distanceMap.get(nodes[0]));
+//            swap(0,size - 1);
+//            heapIndexMap.put(nodes[size - 1],-1);
+//            distanceMap.remove(nodes[size - 1]);
+//            nodes[size - 1] = null;
+//            heapify(0,--size);
+//            return nodeRecord;
+//        }
+//
+//        private void insertHeapify(Node node,int index) {
+//            while(distanceMap.get(nodes[index]) < distanceMap.get(nodes[(index - 1 ) / 2])) {
+//                swap(index,(index - 1) / 2);
+//                index = (index - 1) / 2;
+//            }
+//        }
+//
+////        private void heapify(int index,int size) {
+////            int left = index * 2 + 1;
+////            while(left < size) {
+////                int smallest = left + 1 < size && distanceMap.get(nodes[left + 1] < )
+////            }
+////        }
+//
+//        private boolean isEntered(Node node) {//  node是否进入过堆
+//            return heapIndexMap.containsKey(node);
+//        }
+//
+//        private boolean inHeap(Node node) { // node 是否在堆上
+//            return isEntered(node) && heapIndexMap.get(node) != -1;
+//        }
+//
+//        private void swap(int index1,int index2) {
+//            heapIndexMap.put(nodes[index1],index2);
+//            heapIndexMap.put(nodes[index2],index1);
+//            Node temp = nodes[index1];
+//            nodes[index1] = nodes[index2];
+//            nodes[index2] = temp;
+//        }
+//    }
+//
+//// 改进后的dijkstra算法
+//    public static HashMap<Node,Integer> dijkstra2(Node head,int size) {
+//        NodeHeap nodeHeap = new NodeHeap(size);
+//        nodeHeap.addOrUpdateOrIgnore(head,0);
+//        HashMap<Node,Integer> result = new HashMap<>();
+//        while(!nodeHeap.isEmpty()) {
+//            NodeRecord record = nodeHeap.pop();
+//            Node cur = record.node;
+//            int distance = record.ditance;
+//            for(Edge edge : cur.edges) {
+//                nodeHeap.addOrUpdateOrIgnore(edge.to,edge.weight + distance);
+//            }
+//            result.put(cur,distance);
+//        }
+//        return result;
+//    }
 }
---
+++ coding/src/violence_recursive.java
@@ -0,0 +1,2 @@
+public class violence_recursive {
+}
