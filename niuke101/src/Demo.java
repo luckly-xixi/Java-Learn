f478002e5b80b8d2dd2ae21429edfd8567fbe976
xixi <1348673083@qq.com>
2023-12-19 22:07:19 +0800

    每日一题

--- niuke101/src/Demo.java
+++ niuke101/src/Demo.java
@@ -1540,4 +1540,39 @@ public class Demo {
         return null;
     }
 
+    import java.util.*;
+    public class Solution {
+        public TreeNode Convert(TreeNode pRootOfTree) {
+            if (pRootOfTree == null)
+                return null;
+            //设置栈用于遍历
+            Stack<TreeNode> s = new Stack<TreeNode>();
+            TreeNode head = null;
+            TreeNode pre = null;
+            //确认第一个遍历到最左，即为首位
+            boolean isFirst = true;
+            while(pRootOfTree != null || !s.isEmpty()){
+                //直到没有左节点
+                while(pRootOfTree != null){
+                    s.push(pRootOfTree);
+                    pRootOfTree = pRootOfTree.left;
+                }
+                pRootOfTree = s.pop();
+                //最左元素即表头
+                if(isFirst){
+                    head = pRootOfTree;
+                    pre = head;
+                    isFirst = false;
+                    //当前节点与上一节点建立连接，将pre设置为当前值
+                }else{
+                    pre.right = pRootOfTree;
+                    pRootOfTree.left = pre;
+                    pre = pRootOfTree;
+                }
+                pRootOfTree = pRootOfTree.right;
+            }
+            return head;
+        }
     }
+
+}
