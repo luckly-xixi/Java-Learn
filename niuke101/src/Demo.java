39283ac244aefd1797521d3b797759dc511820a8
xixi <1348673083@qq.com>
2023-12-21 21:47:47 +0800

    每日一题

--- niuke101/src/Demo.java
+++ niuke101/src/Demo.java
@@ -1628,4 +1628,27 @@ public class Demo {
     }
 
 
+    // 32.合并二叉树
+    // 递归
+    public TreeNode mergeTrees1 (TreeNode t1, TreeNode t2) {
+        if(t1 == null) {
+            return t2;
+        }
+        if(t2 == null) {
+            return t1;
+        }
+
+        TreeNode head = new TreeNode(t1.val + t2.val);
+        head.left = mergeTrees(t1.left, t2.left);
+        head.right = mergeTrees(t1.right, t2.right);
+
+        return head;
+    }
+
+
+    public TreeNode mergeTrees (TreeNode t1, TreeNode t2) {
+        
+    }
+
+
     }
