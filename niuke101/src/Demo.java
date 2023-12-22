ef8f8f53a3f66e26c810c7182d98f8c753436e9a
xixi <1348673083@qq.com>
2023-12-22 17:58:09 +0800

    每日一题

--- niuke101/src/Demo.java
+++ niuke101/src/Demo.java
@@ -1647,7 +1647,16 @@ public class Demo {
 
 
     public TreeNode mergeTrees (TreeNode t1, TreeNode t2) {
-        
+        if(t1 == null) {
+            return t2;
+        }
+        if(t2 == null) {
+            return t1;
+        }
+
+        TreeNode head = new TreeNode(t1.val + t2.val);
+
+        return head;
     }
 
 
