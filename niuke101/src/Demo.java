9d4c3c9160fea9ea8b7da739bfcabed3ec028db3
xixi <1348673083@qq.com>
2024-01-12 23:02:34 +0800

    每日一题

--- niuke101/src/Demo.java
+++ niuke101/src/Demo.java
@@ -1921,7 +1921,7 @@ public class Demo {
         return path;
     }
 
-    public int lowestCommonAncestor (TreeNode root, int p, int q) {
+    public int lowestCommonAncestor2 (TreeNode root, int p, int q) {
         ArrayList<Integer> path_p = getPath(root, p);
         ArrayList<Integer> path_q = getPath(root, q);
 
@@ -1941,5 +1941,62 @@ public class Demo {
     }
 
 
+    //38. 在二叉树中找到两个节点的最近公共祖先
+    // 非递归写法
+    public int lowestCommonAncestor3 (TreeNode root, int o1, int o2) {
+        Map<Integer, Integer> parent = new HashMap<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        parent.put(root.val, Integer.MAX_VALUE);
+        queue.add(root);
 
-}
+        while(!parent.containsKey(o1) || !parent.containsKey(o2)) {
+            TreeNode node = queue.poll();
+            if(node.left != null) {
+                parent.put(node.left.val, node.val);
+                queue.add(node.left);
+            }
+
+            if(node.right != null) {
+                parent.put(node.right.val, node.val);
+                queue.add(node.right);
+            }
+        }
+
+        Set<Integer> ancestors = new HashSet<>();
+        while(parent.containsKey(o1)) {
+            ancestors.add(o1);
+            o1 = parent.get(o1);
+        }
+
+        while(!ancestors.contains(o2)) {
+            o2 = parent.get(o2);
+        }
+        return o2;
+    }
+
+
+    // 递归
+
+    private TreeNode helper(TreeNode root, int o1, int o2){
+        if(root == null || root.val == o1 || root.val == o2) {
+            return root;
+        }
+
+        TreeNode left = helper(root.left, o1, o2);
+        TreeNode right = helper(root.right, o1, o2);
+
+        if(left == null) {
+            return right;
+        }
+        if(right == null) {
+            return left;
+        }
+
+        return root;
+    }
+
+    public int lowestCommonAncestor (TreeNode root, int o1, int o2) {
+        return helper(root, o1, o2).val;
+    }
+
+    }
