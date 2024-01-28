cae742c3745f6731c80de6128514232d3aa0ef62
xixi <1348673083@qq.com>
2024-01-28 22:34:33 +0800

    每日一题

--- niuke101/src/Demo.java
+++ niuke101/src/Demo.java
@@ -2374,5 +2374,28 @@ public class Demo {
 
 
     // 42.  用两个栈实现队列
+    // 双栈
+    public class Solution {
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+
+        public void push(int node) {
+            stack1.push(node);
+        }
+
+        public int pop() {
+            while(!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+
+            int res = stack2.pop();
+
+            while(!stack2.isEmpty()) {
+                stack1.push(stack2.pop());
+            }
+
+            return res;
+        }
+    }
 
 }
