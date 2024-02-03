67703460cf187979eb1c3b015f91f74e480539e7
xixi <1348673083@qq.com>
2024-02-03 22:54:23 +0800

    每日一题

--- niuke101/src/Demo.java
+++ niuke101/src/Demo.java
@@ -2589,16 +2589,4 @@ public class Demo {
     }
 
     //
-    public ArrayList<Integer> GetLeastNumbers_Solution2(int[] input, int k) {
-        ArrayList<Integer> res = new ArrayList<>(k);
-        //根据题意要求，如果K>数组的长度，返回一个空的数组
-        if (k > input.length)
-            return res;
-        //先排序，然后选择前k个即可
-        Arrays.sort(input);
-        for (int i = 0; i < k; ++i) {
-            res.add(input[i]);
-        }
-        return res;
-    }
     }
