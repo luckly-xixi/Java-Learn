5b275cfb19b27f2d033648d1449d9144bff7862e
xixi <1348673083@qq.com>
2024-02-15 23:35:09 +0800

    每日一题

--- niuke101/src/Demo.java
+++ niuke101/src/Demo.java
@@ -2692,4 +2692,39 @@ public class Demo {
     }
 
 
+    // 随机patition
+    public int findKth (int[] a, int n, int K) {
+        return quickSort(a, 0, a.length-1, K);
+    }
+
+    private int quickSort(int[] arr, int left, int right, int k) {
+        int p = partition(arr, left, right);
+
+        if(p == arr.length-k) {
+            return arr[p];
+        } else if(p < arr.length-k) {
+            return quickSort(arr, p+1, right, k);
+        } else {
+            return quickSort(arr, left, p-1, k);
+        }
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        int key = arr[left];
+
+        while(left < right) {
+            while(left < right && arr[right] >= key) {
+                right--;
+            }
+            arr[left] = arr[right];
+            while(left < right && arr[left] <= key) {
+                left++;
+            }
+            arr[right] = arr[left];
+        }
+        arr[left] = key;
+        return left;
+    }
+
+
     }
